5b0f21acd86b3e2da90141c5132e418a09cf9047
Merlinkk <anshulkashyap2504@gmail.com>
2024-01-11 11:27:02 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import imageData from "/src/components/dataCompomnent.jsx";
+
+
+function App() {
+  let imgData = imageData();
+  // code here
+  return (
+    <>
+      <h1>Kalvium Gallary - App.jsx</h1>
+      <div className="container">
+        {/* use map to traverse throught the array */}
+        {imgData.map((e) => {
+          return <img key={e.id} src={e.img} alt="" />;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+base: "/[lab-react-jsx]/"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react()],
+  };
+});
